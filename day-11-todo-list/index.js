de8e3bf69304b96c82122709e94d8a6e62ea3bf6
Bahare <bahareahmadi506@gmail.com>
2025-01-15 19:12:06 +0100

    add todo list

---
+++ day-11-todo-list/index.js
@@ -0,0 +1,62 @@
+let inputButton = document.querySelector("#input");
+let addButton = document.querySelector("#add-btn");
+let list = document.querySelector(".list");
+let todoList = [];
+addButton.addEventListener("click", () => {
+  addItemList();
+  renderList();
+});
+inputButton.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addItemList();
+    renderList();
+  }
+});
+
+function renderList() {
+  list.innerHTML = "";
+  todoList.forEach((value, index) => {
+    const li = document.createElement("li");
+    li.textContent = value.text;
+    li.classList.add("list-item-wrapper");
+
+    if (value.state === "done") {
+      li.classList.add("done");
+    }
+    li.addEventListener("click", () => {
+      value.state = value.state === "todo" ? "done" : "todo"; // Taghyir state
+      renderList(); // List ra dobare render mikonim
+      
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("button");
+    deleteButton.textContent = "X";
+    deleteButton.addEventListener("click", () => {
+      deleteTodo(index); // Har item ro ba id hazf mikonim
+    });
+
+    li.appendChild(deleteButton); // Dokmeh ro be item ezafe mikonim
+    list.appendChild(li); // Har li ro be ul list ezafe mikonim
+  });
+}
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  renderList();
+}
+
+function completeTodo(index) {
+  todoList[index].state = "done";
+
+  renderList();
+}
+function addItemList() {
+  const inputValue = inputButton.value;
+
+  todoList.push({
+    text: inputValue,
+    state: "todo",
+  });
+  console.log(todoList);
+  inputButton.value = "";
+}
---
+++ my-app/src/component/MyComponent.js
@@ -0,0 +1,25 @@
+import React, { Component, useLayoutEffect } from "react";
+
+
+/* export default class MyComponent extends Component {
+  state = {
+    firstName: "Bahare",
+    lastName: "Ahmadi",
+    age: 30,
+    location: "Germany",
+  };
+  render() {
+    const { state } = this;
+    return (
+      <ul>
+        {Object.keys(state).map((key) => (
+          <li key={key}>
+            <strong>
+              {key}   : {state[key]}
+            </strong>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+} */
